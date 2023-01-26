3dc4add46917c691747584ee13e41c0703f3501e
torres30 <tomastorresdiaz01@gmail.com>
2023-01-25 21:21:31 -0300

    components structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { MainComponent } from './main/main.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ToolbarComponent,
+    MainComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
